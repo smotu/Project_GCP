1c8f8b572609cbf67b9f35e1e5dc4000c296e35a
Jenkins <jenkins@ip-172-31-81-30.ec2.internal>
2023-09-27 21:03:22 +0000

    v2

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "docker_image" "apache" {
   name = "apache"
   build {
     context = "."
-    tag     = ["apache:test"]
+    tag     = ["mishras95/apache:test"]
 #    build_arg = {
 #      foo : "" 
 #    }
@@ -35,7 +35,7 @@ resource "google_service_account" "default" {
 
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
-  location = "us-central1"
+  location = "asia-south1-a"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
@@ -43,4 +43,67 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 }
+
+resource "kubernetes_deployment" "apache" {
+  metadata {
+    name = "apache"
+    labels = {
+      App = "apache"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = ""apache""
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = ""apache""
+        }
+      }
+      spec {
+        container {
+          image = "mishras95/apache:test"
+          name  = "apache"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+resource "kubernetes_service" "apache-lb" {
+  metadata {
+    name = "apachelb"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.apache.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
 
